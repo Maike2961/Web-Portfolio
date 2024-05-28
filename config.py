7eee4f9c008472e7d47b3eb62566de1720fe5e98
Maike2961 <mayki.silva@hotmail.com>
2024-05-28 17:56:42 -0300

    Função email funcionando e add o flask

---
+++ config.py
@@ -0,0 +1,10 @@
+from app import app
+import os
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
+app.config['MAIL_PORT'] = os.getenv("MAIL_PORT")
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
